8d84fe5432a78ffd2209f55922d2e4ad7651cb19
tusharkumardas <tusharkumardas1125@gmail.com>
2025-04-09 12:10:48 +0530

    added Script.js

---
+++ even.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int even (int n )
+{
+    if(n%2==0)
+
+}
+int main (){
+    int n ;
+    cin >> n;
+    cout<< " even number"<<endl;
+
+    return n;
+}
--- script.js
+++ script.js
@@ -12,8 +12,8 @@ let apiResponse = false;
 
 //API Configuration
 
-const API_KEY = "";
-const API_URL = `${API_KEY}`;
+const API_KEY = "AIzaSyAP6ena5cWK6pnkR4JBZOUxpwAomtawHXQ";
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
 
 // saving the theme in the local storage
 
